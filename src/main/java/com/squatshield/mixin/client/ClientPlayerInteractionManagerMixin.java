5fb01cfaed8d9f47bffc5ba19071e91b6a3b4d8f
tiancaixiaolanxiaolan-maker <tiancaixiaolan.xiaolan@gmail.com>
2026-09-20 02:58:28 +0800

    Add ClientPlayerInteractionManagerMixin

---
+++ src/main/java/com/squatshield/mixin/client/ClientPlayerInteractionManagerMixin.java
@@ -0,0 +1,24 @@
+package com.squatshield.mixin.client;
+
+import com.squatshield.input.SneakShieldContext;
+import net.minecraft.client.multiplayer.MultiPlayerGameMode;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ShieldItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MultiPlayerGameMode.class)
+public class ClientPlayerInteractionManagerMixin {
+    @Inject(method = "useItem", at = @At("HEAD"), cancellable = true)
+    private void squatshield$blockRightClickShield(Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResult> cir) {
+        if (SneakShieldContext.isSneakTriggered()) return;
+
+        ItemStack stack = player.getItemInHand(hand);
+        if (stack.getItem() instanceof ShieldItem) cir.setReturnValue(InteractionResult.PASS);
+    }
+}
