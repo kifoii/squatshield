2984f4774211ebaeec483c1c8ec7cc6c659b8321
kifoii <230928687+kifoii@users.noreply.github.com>
2026-09-21 16:03:46 +0800

    Add core SquatShield initializer

---
+++ src/main/java/com/squatshield/SquatShield.java
@@ -0,0 +1,16 @@
+package com.squatshield;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SquatShield implements ModInitializer {
+    public static final String MOD_ID = "squatshield";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        SquatShieldConfig.load();
+        LOGGER.info("SquatShield initialized");
+    }
+}
