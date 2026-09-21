2a892a18c6f97415797ed03df2b45dc729d81d27
kifoii <230928687+kifoii@users.noreply.github.com>
2026-09-21 16:32:00 +0800

    Port client key mapping to Minecraft 26.2 API

--- src/main/java/com/squatshield/SquatShieldClient.java
+++ src/main/java/com/squatshield/SquatShieldClient.java
@@ -3,9 +3,8 @@ package com.squatshield;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
 import net.minecraft.client.KeyMapping;
-import net.minecraft.client.Minecraft;
 import org.lwjgl.glfw.GLFW;
 
 public class SquatShieldClient implements ClientModInitializer {
@@ -13,17 +12,21 @@ public class SquatShieldClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        openMenu = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+        KeyMapping.Category category = KeyMapping.Category.register(
+            SquatShield.MOD_ID + ":main"
+        );
+
+        openMenu = KeyMappingHelper.registerKeyMapping(new KeyMapping(
             "key.squatshield.open_menu",
             InputConstants.Type.KEYSYM,
             GLFW.GLFW_KEY_O,
-            "category.squatshield"
+            category
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (openMenu.consumeClick()) {
-                if (client.screen == null) {
-                    client.setScreen(new SquatShieldScreen(null));
+                if (client.gui.screen() == null) {
+                    client.gui.setScreen(new SquatShieldScreen(null));
                 }
             }
         });
